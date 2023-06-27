1247bfb234b22ee5d5c2d7a54fee1b028a441cb4
Zhangyrkhan <136449165+Zhangyrkhan@users.noreply.github.com>
2023-06-27 12:08:29 +0600

    START

---
+++ src/main/java/com/example/MebelMarket/MebelMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.MebelMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MebelMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MebelMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MebelMarket/MebelMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MebelMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MebelMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
